3bd407b69ac2270b2dcfbd2c8991a1e7f41cc62d
yehtest2 <yeahajson99@gmail.com>
2022-06-10 14:56:18 +0800

    Updates

---
+++ precache-manifest.43158c108af85e63bb1e1d762b43d1e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "23ec663030d96ca38e0f",
+    "url": "/robo/static/css/main.d4c0b61c.chunk.css"
+  },
+  {
+    "revision": "23ec663030d96ca38e0f",
+    "url": "/robo/static/js/main.23ec6630.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/robo/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "3e8664ae2e0e85139de4",
+    "url": "/robo/static/css/2.ce896fdd.chunk.css"
+  },
+  {
+    "revision": "3e8664ae2e0e85139de4",
+    "url": "/robo/static/js/2.3e8664ae.chunk.js"
+  },
+  {
+    "revision": "59355af31cd9471df7f7",
+    "url": "/robo/static/js/runtime~main.59355af3.js"
+  },
+  {
+    "revision": "65d3ffb9e8fff4bab10947edd8284215",
+    "url": "/robo/index.html"
+  }
+];
